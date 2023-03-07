3d4237fceb12cd02ea3a622de7bebb76838c6495
to_binio <tobias.frischmann1@gmail.com>
2023-03-07 16:39:07 +0100

    add cors

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,3 +1,4 @@
+import cors from "cors";
 import * as dotenv from "dotenv";
 import express from "express";
 import {getUserRouter} from "./user/user.router";
@@ -10,6 +11,9 @@ let server = express()
 
 //enable json in body
 server.use(express.json())
+
+//enable cors
+server.use(cors())
 
 //default response
 server.get("/", (req, res) => {
