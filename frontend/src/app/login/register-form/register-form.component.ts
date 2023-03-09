31ed0bf80478f5de667700332658fdbb4122a8bf
benni043 <benni.huff@gmail.com>
2023-03-09 20:08:42 +0100

    error handling register

--- frontend/src/app/login/register-form/register-form.component.ts
+++ frontend/src/app/login/register-form/register-form.component.ts
@@ -1,5 +1,4 @@
 import {Component} from '@angular/core';
-import {Observable} from "rxjs";
 import {HttpClient} from "@angular/common/http";
 import {UserLoginRequest} from "../../../../../types/user";
 import {hashPassword} from "../loginUtil";
@@ -28,19 +27,33 @@ export class RegisterFormComponent {
 
   async register() {
     if (!this.checkPasswordCorrectness(this.password, this.passwordCheck)) {
-      alert("the passwords are different")
+      console.log("the passwords are different");
       return;
     }
 
     let passwordHash = await hashPassword(this.password);
 
-    this.sendDataToBackend({userName: this.userName, password: passwordHash} as UserLoginRequest)
-      .subscribe(res => {
-        this.token = res;
-      })
+    this.httpClient.post<string>(this.baseURL, {
+      userName: this.userName,
+      password: passwordHash
+    } as UserLoginRequest, {responseType: "text" as 'json'})
+      .subscribe(
+        res => {
+          console.log("ok")
+          this.token = res;
+        },
+        error => {
+          switch (error.status) {
+            case 403: {
+              console.log("user already in database");
+              break;
+            }
+            default: {
+              console.log("something went wrong");
+            }
+          }
+        }
+      )
   }
 
-  sendDataToBackend(userLoginRequest: UserLoginRequest): Observable<string> {
-    return this.httpClient.post<string>(this.baseURL, userLoginRequest, {responseType: "text" as 'json'});
-  }
 }
