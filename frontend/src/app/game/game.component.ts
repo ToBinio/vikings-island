48f12bacf59e6218a5294f9b8677f0873172b245
to_binio <tobias.frischmann1@gmail.com>
2023-05-27 21:22:32 +0200

    minor fixes

--- frontend/src/app/game/game.component.ts
+++ frontend/src/app/game/game.component.ts
@@ -7,50 +7,54 @@ import {Component, ElementRef, OnInit, ViewChild} from '@angular/core';
 })
 export class GameComponent implements OnInit {
 
-
-  cameraOffset = {x: window.innerWidth / 2, y: window.innerHeight / 2}
-  cameraZoom = 1
-  MAX_ZOOM = 5
-  MIN_ZOOM = 0.1
-  SCROLL_SENSITIVITY = 0.01
-
   @ViewChild('gameCanvas', {static: true})
   canvas!: ElementRef<HTMLCanvasElement>;
 
-  constructor() {
-  }
+  MIN_ZOOM = 0.1
+  SCROLL_SENSITIVITY = 0.01
+
+  cameraOffset = {x: window.innerWidth / 2, y: window.innerHeight / 2}
+  cameraZoom = 1
+  lastZoom = this.cameraZoom
+
+  isDragging = false
+  dragStart = {x: 0, y: 0}
+
+  initialPinchDistance: null | number = null
 
   ngOnInit(): void {
 
     let canvas = this.canvas.nativeElement;
 
     canvas.addEventListener('mousedown', (e) => this.onPointerDown(e))
-    canvas.addEventListener('touchstart', (e) => this.handleTouch(e, (e) => this.onPointerDown(e)))
-    canvas.addEventListener('mouseup', (e) => this.onPointerUp(e))
-    canvas.addEventListener('touchend', (e) => this.handleTouch(e, (e) => this.onPointerUp(e)))
+    canvas.addEventListener('mouseup', () => this.onPointerUp())
     canvas.addEventListener('mousemove', (e) => this.onPointerMove(e))
+
+    canvas.addEventListener('touchstart', (e) => this.handleTouch(e, (e) => this.onPointerDown(e)))
+    canvas.addEventListener('touchend', (e) => this.handleTouch(e, () => this.onPointerUp()))
     canvas.addEventListener('touchmove', (e) => this.handleTouch(e, (e) => this.onPointerMove(e)))
 
-    this.canvas.nativeElement.addEventListener('DOMMouseScroll', (e) => {
-      // @ts-ignore
-      let scroll = e.wheelDelta ? e.wheelDelta / 40 : e.detail ? -e.detail : 0;
+    canvas.addEventListener('DOMMouseScroll', (e) => this.handleZoom(e), false);
+    canvas.addEventListener('mousewheel', (e) => this.handleZoom(e), false);
 
-      this.adjustZoom(scroll * this.SCROLL_SENSITIVITY, 1)
-
-      e.preventDefault()
-      return false;
-    }, false);
-    this.canvas.nativeElement.addEventListener('mousewheel', (e) => {
-
-      // @ts-ignore
-      let scroll = e.wheelDelta ? e.wheelDelta / 40 : e.detail ? -e.detail : 0;
-
-      this.adjustZoom(scroll * this.SCROLL_SENSITIVITY, 1)
-      e.preventDefault()
-      return false;
-    }, false);
+    window.addEventListener("resize", () => this.redraw());
 
     this.redraw();
+  }
+
+  render(ctx: CanvasRenderingContext2D) {
+    //render checker
+    for (let x = 0; x < 10; x++) {
+      for (let y = 0; y < 10; y++) {
+        if ((x + y) % 2) {
+          ctx.fillStyle = "gray";
+        } else {
+          ctx.fillStyle = "black";
+        }
+
+        ctx.fillRect(x * 100, y * 100, 100, 100);
+      }
+    }
   }
 
   redraw() {
@@ -66,18 +70,7 @@ export class GameComponent implements OnInit {
 
     ctx.clearRect(0, 0, window.innerWidth, window.innerHeight)
 
-    //render checker
-    for (let x = 0; x < 10; x++) {
-      for (let y = 0; y < 10; y++) {
-        if ((x + y) % 2) {
-          ctx.fillStyle = "gray";
-        } else {
-          ctx.fillStyle = "black";
-        }
-
-        ctx.fillRect(x * 100, y * 100, 100, 100);
-      }
-    }
+    this.render(ctx)
   }
 
   getEventLocation(e: any) {
@@ -88,9 +81,6 @@ export class GameComponent implements OnInit {
     }
   }
 
-  isDragging = false
-  dragStart = {x: 0, y: 0}
-
   onPointerDown(e: Event) {
     this.isDragging = true
 
@@ -98,7 +88,7 @@ export class GameComponent implements OnInit {
     this.dragStart.y = this.getEventLocation(e).y / this.cameraZoom - this.cameraOffset.y
   }
 
-  onPointerUp(e: Event) {
+  onPointerUp() {
     this.isDragging = false
     this.initialPinchDistance = null
     this.lastZoom = this.cameraZoom
@@ -122,16 +112,12 @@ export class GameComponent implements OnInit {
     }
   }
 
-  initialPinchDistance: null | number = null
-  lastZoom = this.cameraZoom
-
   handlePinch(e: any) {
     e.preventDefault()
 
     let touch1 = {x: e.touches[0].clientX, y: e.touches[0].clientY}
     let touch2 = {x: e.touches[1].clientX, y: e.touches[1].clientY}
 
-    // This is distance squared, but no need for an expensive sqrt as it's only used in ratio
     let currentDistance = (touch1.x - touch2.x) ** 2 + (touch1.y - touch2.y) ** 2
 
     if (this.initialPinchDistance == null) {
@@ -141,7 +127,16 @@ export class GameComponent implements OnInit {
     }
   }
 
-  adjustZoom(zoomAmount: number | null, zoomFactor: number) {
+  handleZoom(e: any) {
+    // @ts-ignore
+    let scroll = e.wheelDelta ? e.wheelDelta / 40 : e.detail ? -e.detail : 0;
+
+    this.adjustZoom(scroll * this.SCROLL_SENSITIVITY, null)
+    e.preventDefault()
+    return false;
+  }
+
+  adjustZoom(zoomAmount: number | null, zoomFactor: number | null) {
     if (!this.isDragging) {
       if (zoomAmount) {
         this.cameraZoom += zoomAmount
@@ -149,7 +144,6 @@ export class GameComponent implements OnInit {
         this.cameraZoom = zoomFactor * this.lastZoom
       }
 
-      this.cameraZoom = Math.min(this.cameraZoom, this.MAX_ZOOM)
       this.cameraZoom = Math.max(this.cameraZoom, this.MIN_ZOOM)
 
       this.redraw()
