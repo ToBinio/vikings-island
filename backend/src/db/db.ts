a752e02ca438b45d2acb619470c7c99c51aaf848
ToBinio <tobias.frischmann1@gmail.com>
2023-06-03 14:04:29 +0200

    add database schema

--- backend/src/db/db.ts
+++ backend/src/db/db.ts
@@ -28,10 +28,22 @@ export interface PlayerTable {
     color: string,
 }
 
+export interface ShipTable {
+    id: Generated<number>,
+    user_id: number,
+    game_id: number,
+
+    x: number,
+    y: number,
+    goal_x: Generated<number>,
+    goal_y: Generated<number>
+}
+
 interface Database {
     users: UserTable
     games: GameTable
     players: PlayerTable
+    ships: ShipTable
 }
 
 export const db = new Kysely<Database>({
